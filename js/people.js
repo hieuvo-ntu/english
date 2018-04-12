684138f5173a5ebd0b16a9051a4a4bc049f06b2d
hieuvo-ntu <hieu.v.56ctt@ntu.edu.vn>
2018-04-12 11:40:39 +0700

    edited

---
+++ js/people.js
@@ -0,0 +1,93 @@
+var emotions=[
+    {
+        vocab:'Anxious',
+        mean:'lo lắng',
+        pronun:'/ˈæŋk.ʃəs/',
+        link:'https://dictionary.cambridge.org/media/english/us_pron/a/anx/anxio/anxious.mp3',
+    },
+    {
+        vocab:'Bored',
+        mean:'lo lắng',
+        pronun:'/bɔːrd/',
+        link:'https://dictionary.cambridge.org/media/english/us_pron/b/boa/board/board.mp3',
+    },
+    {
+        vocab:'Confident',
+        mean:'tự tin',
+        pronun:'/ˈkɒn.fɪ.dənt/',
+        link:'https://dictionary.cambridge.org/media/english/uk_pron/u/ukc/ukcon/ukcondu025.mp3',
+    },
+    {
+        vocab:'Confused',
+        mean:'bối rối, lẫn lộn, rối trí',
+        pronun:'/kənˈfjuːz/',
+        link:'https://dictionary.cambridge.org/media/english/uk_pron/u/ukc/ukcon/ukconfi028.mp3',
+    },
+    {
+        vocab:'Curious',
+        mean:'hiếu kì',
+        pronun:'/ˈkjʊr.i.əs/',
+        link:'https://dictionary.cambridge.org/media/english/us_pron/c/cur/curio/curious.mp3',
+    },
+    {
+        vocab:'Depressed',
+        mean:'suy sụp',
+        pronun:'/dɪˈprest/',
+        link:'https://dictionary.cambridge.org/media/english/us_pron/d/dep/depre/depressed.mp3',
+    },
+    {
+        vocab:'Disgusted',
+        mean:'chán ghét',
+        pronun:'/dɪsˈɡʌst/',
+        link:'https://dictionary.cambridge.org/media/english/us_pron/d/dis/disgu/disgust.mp3',
+    },
+    {
+        vocab:'Embarrassed',
+        mean:'xấu hổ',
+        pronun:'/ɪmˈber.əs/',
+        link:'https://dictionary.cambridge.org/media/english/us_pron/e/eus/eus71/eus71500.mp3',
+    },
+    {
+        vocab:'Envious',
+        mean:'ghen tị, ghen ghét',
+        pronun:'/ˈen.vi.əs/',
+        link:'https://dictionary.cambridge.org/media/english/us_pron/e/env/envio/envious.mp3',
+    },
+    {
+        vocab:'Excited',
+        mean:'bị kích thích',
+        pronun:'/ɪkˈsaɪt/',
+        link:'https://dictionary.cambridge.org/media/english/us_pron/e/exc/excit/excite.mp3',
+    },
+    {
+        vocab:'Irritated',
+        mean:'',
+        pronun:'/ˈɪr.ɪ.teɪ.tɪd/',
+        link:'https://dictionary.cambridge.org/media/english/uk_pron/u/ukc/ukcld/ukcld00752.mp3',
+    },
+    {
+        vocab:'Scared',
+        mean:'sợ hãi, khiếp sợ',
+        pronun:'/skerd/',
+        link:'https://dictionary.cambridge.org/media/english/us_pron/s/sca/scare/scared.mp3',
+    },
+    {
+        vocab:'Surprised',
+        mean:'ngạc nhiên',
+        pronun:'/sɚˈpraɪzd/',
+        link:'https://dictionary.cambridge.org/media/english/us_pron/s/sur/surpr/surprised.mp3',
+    },
+    {
+        vocab:'Suspicious',
+        mean:'nghi ngờ, khả nghi',
+        pronun:'/səˈspɪʃ.əs/',
+        link:'https://dictionary.cambridge.org/media/english/us_pron/s/sus/suspi/suspicious.mp3',
+    },
+    {
+        vocab:'Upset',
+        mean:'buồn bã, súy sụp',
+        pronun:'/ʌpˈset/ ',
+        link:'https://dictionary.cambridge.org/media/english/us_pron/u/ups/upset/upset.mp3',
+    },
+
+];
